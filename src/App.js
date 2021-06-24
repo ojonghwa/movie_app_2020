efe84b47b2398eb87f2ad92006ddfc67a82ada3d
ojonghwa <ojonghwa@naver.com>
2021-06-25 08:09:52 +0900

    07-2 영화 앱 스타일

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     //return <div>{ isLoading ? 'Loding...' : 'We are ready' }</div>;
-    return <section class="container">
+    return <section className="container">
       { isLoading ? (
-          <div class="loader">
-            <span class="loader__text">'Loding...'</span>  
+          <div className="loader">
+            <span className="loader__text">'Loding...'</span>  
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             { movies.map((movie) => (
               //console.log(movie);
 
@@ -45,6 +45,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
 
               ))
